48536ac07758f2b8b12e3d6c76fa65e7826e2e44
ekgpgdi <dahae80912@gmail.com>
2024-12-12 12:27:44 +0900

    🎉 init commit

---
+++ src/main/java/com/example/wantedpreonboardingchallengebackend/WantedPreonboardingChallengeBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.wantedpreonboardingchallengebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedPreonboardingChallengeBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WantedPreonboardingChallengeBackendApplication.class, args);
+  }
+}
---
+++ src/test/java/com/example/wantedpreonboardingchallengebackend/WantedPreonboardingChallengeBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.wantedpreonboardingchallengebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedPreonboardingChallengeBackendApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
